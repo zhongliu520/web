f88f5c4e610fadb4abfbfa6cdd99aa7e1ce7fef6
zhong.liu <597764553@qq.com>
2018-07-28 15:53:51 +0800

    update

---
+++ app/Models/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhong
+ * Date: 2018/7/28
+ * Time: 14:31
+ */
+
+namespace App\Models;
+
+
+class User extends Model
+{
+
+}
--- resources/views/myself/index.blade.php
+++ resources/views/myself/index.blade.php
@@ -32,11 +32,44 @@
         height: 700px;
     }
 
+    .img_li{
+        width: 33%;
+        float: left;
+    }
+
 </style>
 <body>
 <div class="main">
     <div class="top"></div>
-    <div class="body"></div>
+    <div class="body">
+        <input type="file" accept="image/gif,image/jpeg,image/jpg,image/png,image/svg" onchange="upImg(this)" />
+        <div class="img_list">
+
+
+        </div>
+    </div>
 </div>
+
 </body>
 </html>
+
+<script>
+
+    function upImg(obj){
+        var imgFile = obj.files[0];
+        console.log(imgFile);
+        var img = new Image();
+        var fr = new FileReader();
+
+        console.log(fr.result);
+        fr.onload = function(){
+            var tag = document.getElementsByClassName("img_list")[0].innerHTML;
+
+            tag += "<div class=\"img_li\"><img src=\""+fr.result+"\" /></div>";
+
+            document.getElementsByClassName("img_list")[0].innerHTML = tag;
+            console.log(fr.result);
+        }
+        fr.readAsDataURL(imgFile);
+    }
+</script>
