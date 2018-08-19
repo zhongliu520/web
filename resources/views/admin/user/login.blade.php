143748e659c54395691478d3163a9747689d4764
zhong.liu <zhong.liu@bioon.com>
2018-08-19 23:34:48 +0800

    update

--- resources/views/admin/user/login.blade.php
+++ resources/views/admin/user/login.blade.php
@@ -8,7 +8,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Metronic | User Login 1</title>
+    <title>后台管理系统</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <meta content="" name="description" />
