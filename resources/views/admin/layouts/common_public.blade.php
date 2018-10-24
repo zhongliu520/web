c887165cd59bbe3341e00254f7b79d4155501960
zhao <287930879@qq.com>
2018-10-24 18:15:24 +0800

    update

--- resources/views/admin/layouts/common_public.blade.php
+++ resources/views/admin/layouts/common_public.blade.php
@@ -81,7 +81,7 @@
     <script src="/admin/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js" type="text/javascript"></script>
     <script src="/admin/assets/global/plugins/morris/morris.min.js" type="text/javascript"></script>
     <script src="/admin/assets/global/plugins/morris/raphael-min.js" type="text/javascript"></script>
-    <script src="/admin/assets/global/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+    {{--<script src="/admin/assets/global/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>--}}
     <script src="/admin/assets/global/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
     <script src="/admin/assets/global/plugins/amcharts/amcharts/amcharts.js" type="text/javascript"></script>
     <script src="/admin/assets/global/plugins/amcharts/amcharts/serial.js" type="text/javascript"></script>
