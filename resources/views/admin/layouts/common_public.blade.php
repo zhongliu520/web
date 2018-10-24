254280ebdbd7cae42b4e1e445bf2a37fd9ef7eac
zhao <287930879@qq.com>
2018-10-24 18:19:30 +0800

    update

--- resources/views/admin/layouts/common_public.blade.php
+++ resources/views/admin/layouts/common_public.blade.php
@@ -73,7 +73,7 @@
 @section('foot')
     <!-- BEGIN PAGE LEVEL PLUGINS -->
     <script src="/admin/assets/global/scripts/datatable.js" type="text/javascript"></script>
-    <script src="/admin/assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
+    {{--<script src="/admin/assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>--}}
     <script src="/admin/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
     <!-- END PAGE LEVEL PLUGINS -->
     <!-- BEGIN PAGE LEVEL PLUGINS -->
