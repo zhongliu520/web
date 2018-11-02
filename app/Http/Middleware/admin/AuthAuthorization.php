a517d75351a15b2a8d98a3cc8d8e89da17d00084
zhao <287930879@qq.com>
2018-11-02 18:39:59 +0800

    updat

---
+++ app/Http/Middleware/admin/AuthAuthorization.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a1
+ * Date: 18/11/2
+ * Time: 17:15
+ */
+
+namespace App\Http\Middleware\admin;
+
+use Closure;
+//use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+
+use Illuminate\Support\Facades\Auth;
+
+class AuthAuthorization
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $user = Auth::guard("admin")->user();
+
+        $user->id;
+
+        return $next($request);
+    }
+
+}
