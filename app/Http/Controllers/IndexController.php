134e5313a4ee62d018dba1e6c8ed4e7572a988ca
zhao <287930879@qq.com>
2019-01-07 09:54:51 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,21 +14,20 @@ class IndexController extends  Controller
 
     public function index()
     {
-        $rows = config("test.test");
-        foreach ($rows as $key=>$item)
-        {
-//            $item = file_get_contents($item);
-//            $myfile = fopen("testfile.txt", "w");
-//            $item = base64_decode($item);
-//            dd($item);
-            $this->down_images($item, ($key+1));
-        }
-        dd($rows);
-        return view("myself/test", ["rows"=>$rows]);
+//        $rows = file_get_contents("http://myself.zhong.liu.com.cn/maotai/bj/0105/index.html");
+//        preg_match_all("/<img([^\<\>]*?)src=\"(.*?)\"([^\<\>]*?)>/", $rows, $rowas);
+//        $rows = $rowas[2];
+//        foreach ($rows as $key=>$item)
+//        {
+////            $item = file_get_contents($item);
+//            $this->down_images($item, $key);
+//        }
+//        dd($rows);
+//        return view("myself/test", ["rows"=>$rows]);
     }
 
 
-    function down_images($url, $key=0) {
+    function down_images($url, $key=null) {
 
 
         $header = array("Connection: Keep-Alive", "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Pragma: no-cache", "Accept-Language: zh-Hans-CN,zh-Hans;q=0.8,en-US;q=0.5,en;q=0.3", "User-Agent: Mozilla/5.0 (Windows NT 5.1; rv:29.0) Gecko/20100101 Firefox/29.0");
@@ -73,7 +72,7 @@ class IndexController extends  Controller
 
             }
 
-            $filename = ($key>0? $key: (date("YmdHis") . uniqid())) . $exf;//这里默认是当前文件夹，可以加路径的 可以改为$filepath = '../'.$filename
+            $filename = ($key != null? $key: (date("YmdHis") . uniqid())) . $exf;//这里默认是当前文件夹，可以加路径的 可以改为$filepath = '../'.$filename
             $dir = __DIR__."/../../../storage/my/images";
             $dir = iconv("UTF-8", "GBK", $dir);
 
